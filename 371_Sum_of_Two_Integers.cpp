6a72a2605a1e3adc60a9b88dac11f8fccd32ab1c
mekks <patelmayank667@gmail.com>
2020-05-12 13:13:03 +0530

    Create 371_Sum_of_Two_Integers.cpp

---
+++ 371_Sum_of_Two_Integers.cpp
@@ -0,0 +1,24 @@
+/*
+Question:
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+Example 1:
+
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+
+Input: a = -2, b = 3
+Output: 1
+*/
+
+//Sol_1 0ms
+class Solution {
+public:
+    int getSum(int a, int b) {
+        if (b==0) return a;
+	int sum = a^b; // finding the sum
+	int carry = (unsigned int)(a & b)<<1; // finding the carry
+	return getSum(sum, carry);
+    }
+};
