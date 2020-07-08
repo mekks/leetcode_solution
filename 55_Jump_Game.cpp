4c53ace6564a66e61d9c9b79c7f165f691dcb59f
mekks <patelmayank667@gmail.com>
2020-07-08 22:21:34 +0530

    Create 55_Jump_Game.cpp

---
+++ 55_Jump_Game.cpp
@@ -0,0 +1,53 @@
+/*
+Question:
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+*/
+//Sol:1 24ms
+/*class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int maxReach =0;
+        
+        for(int i=0 ; i<n ; i++)
+        {
+            if(maxReach < i)
+                return false;
+            maxReach = max(maxReach , i+nums[i]);
+        }
+        return true;
+    }
+};
+*/
+
+//Sol:2 20ms
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int i,minjump=0;
+        for(i=nums.size()-2;i>=0;i--){
+            minjump++;
+            if(nums[i]>=minjump)minjump=0;
+        }
+        if(minjump==0)return true;
+        else return false;
+    }
+};
