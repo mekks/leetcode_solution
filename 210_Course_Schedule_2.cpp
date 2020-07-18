a1656184915e70114b5904290a45f0b289c465e7
mekks <patelmayank667@gmail.com>
2020-07-18 18:13:35 +0530

    Create 210_Course_Schedule_2.cpp

---
+++ 210_Course_Schedule_2.cpp
@@ -0,0 +1,76 @@
+/*
+Question:
+There are a total of n courses you have to take, labeled from 0 to n-1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
+
+There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
+
+Example 1:
+
+Input: 2, [[1,0]] 
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished   
+             course 0. So the correct course order is [0,1] .
+Example 2:
+
+Input: 4, [[1,0],[2,0],[3,1],[3,2]]
+Output: [0,1,2,3] or [0,2,1,3]
+Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both     
+             courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0. 
+             So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3] .
+Note:
+
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
+
+*/
+
+//Sol:1 48ms
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses, 0);
+        queue<int> zeroDegree;   
+        vector<int> course_order;
+        //create graph in form of adjecenet list and find also indegree
+        for(int i=0; i < prerequisites.size();i++)
+        {
+            graph[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++;
+        }
+        
+        //push zero degree nodes into queue
+        for(int i=0;i<indegree.size();i++)
+        {
+            if(indegree[i]==0)
+            {
+                zeroDegree.push(i);
+                numCourses--;
+            }
+        }
+        
+        while(!zeroDegree.empty())
+        {
+            int node = zeroDegree.front();
+            course_order.push_back(node);
+            zeroDegree.pop();
+            for(int i=0;i < graph[node].size();i++)
+            {
+                int connectedNode = graph[node][i];
+                indegree[connectedNode]--;
+                if(indegree[connectedNode] == 0)
+                {
+                    zeroDegree.push(connectedNode);
+                    numCourses--;
+                }
+            }
+        }
+        if(numCourses==0)return course_order;
+        course_order.clear();
+        return course_order;
+    }
+};
