b0e7d05f632e9df80e827ca8e6327cb7f89f1282
mekks <patelmayank667@gmail.com>
2020-08-05 09:27:10 +0530

    Create 287_Find_the_Duplicate_Number.cpp

---
+++ 287_Find_the_Duplicate_Number.cpp
@@ -0,0 +1,112 @@
+/*
+Question:
+
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. 
+Assume that there is only one duplicate number, find the duplicate one.
+
+Example 1:
+
+Input: [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: [3,1,3,4,2]
+Output: 3
+Note:
+
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once
+*/
+
+//Sol:1 40ms
+/*
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+       map<int , int> mp;
+        int ans;
+        for(auto i:nums)
+        {
+            mp[i]++;
+        }
+        for(auto j=mp.begin(); j!=mp.end();j++)
+        {
+            if(j->second >= 2)
+            {
+                ans=j->first;
+                break;
+            }
+        }
+        return ans;
+    }
+};
+*/
+
+//Sol:2 32ms
+/*
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // sort the numbers and find the number which has duplicate
+        // by checking the one which has the same at next position
+        sort(nums.begin(), nums.end());
+        
+        int duplicate = 0;
+        for(int i = 0; i < nums.size() - 1; i++)
+            if(nums[i] == nums[i+1]) {
+                duplicate = nums[i];
+                break;
+            }
+        return duplicate;
+    }
+};
+*/
+//Sol:3 20ms
+/*
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // The idea is that it is known that there is a duplicate, so 
+        // we can use pigeon hole concept here. We do binary search on the search
+        // space of [1:N], then count the number of elements <= middle. If the 
+        // duplicate is on the left side, then count should be more than mid,
+        // else it is on the right side. Initially search space is [1: N], then
+        // each time narrow down the search space
+        int left = 1, right = nums.size() - 1;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            
+            // count the number of elements smaller/ equal than middle element
+            int c = 0;
+            for(const int& el: nums)
+                if(el <= mid)
+                    ++c;
+            
+            if(c > mid)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return left;
+    }
+};
+*/
+
+//Sol:4 12ms
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++) {
+            int index = abs(nums[i]) - 1;
+            
+            // mark as visited
+            nums[index] *= -1;
+            // incase of duplicate, this will become +ve 
+            if(nums[index] > 0)
+                return abs(nums[i]);
+        }    
+        return -1;
+    }
+};
